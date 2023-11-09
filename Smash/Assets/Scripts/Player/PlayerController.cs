8796346e48eb2460f5562c9423d66ce9c179e0ba
Roux Cédric <c.roux@lahorde.school>
2023-11-09 17:38:58 +0100

    Scripts
    -add playerMovement
    -add BasicAttack
    -add RemoteAttack
    -add Parade
    -add InputManager
    -add playerPrefab

---
+++ Smash/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerLife),typeof(PlayerMovement),typeof(BasicAttack))]
+[RequireComponent(typeof(RemoteAttack), typeof(Parade))]
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
