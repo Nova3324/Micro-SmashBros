d3b6bcef6a5e4055a7d5d5ab2ded230c633f189d
TututLaHorde <a.boucher@lahorde.school>
2023-11-10 15:39:48 +0100

    CreatePlayerLifeScript
    -Func take dmg
    -Func knockback
    -Func KickedOut

--- Smash/Assets/Scripts/Player/PlayerLife.cs
+++ Smash/Assets/Scripts/Player/PlayerLife.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int m_life = 3;
+    private int m_damageTaken = 0;
+
+    //knockback
+    private float m_knockbackCoef = 0.01f;
+
+    /*----------------------------------------------------------*/
+
+    public void TakeDamage(int damage)
     {
-        
+        m_damageTaken += damage;
+        m_damageTaken = Mathf.Clamp(m_damageTaken, 0, 999);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void KickedOut()
     {
-        
+        m_life--;
+        m_damageTaken = 0;
+
+
+        if (m_life > 0)
+        {
+            //TODO Respawn
+        }
+        else
+        {
+            //TODO Game Over
+        }
+    }
+
+    /*----------------------------------------------------------*/
+
+    private void Knockback(int damage)
+    {
+        float knockback = Mathf.Pow(m_damageTaken, 2f) * m_knockbackCoef * damage;
+
+        //TODO knockback /= playerMass
     }
 }
